7cab9b899896bcfb35d51a7c4d3f44bd24bda471
Shamsuzzoha <33594007+ArbiKhan007@users.noreply.github.com>
2025-11-22 19:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let selected=document.querySelector("#colorSelect");
+let btn=document.querySelector("#btn");
+
+btn.addEventListener("click", ()=>{
+	selected.remove(selected.selectedIndex);
+});
